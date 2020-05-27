3ee6953fef6bead6db42bf38472c1fc4784ac6c7
DECKER54 <65751513+DECKER54@users.noreply.github.com>
2020-05-27 12:35:01 -0700

    had to

--- comparator.sv
+++
@@ -1,7 +0,0 @@
-module comparator #(parameter N = 8,M = 59)
-							(input logic [N-1: 0] a,
-							output logic q);
-							
-		assign q = ( a >= M );
-		
-endmodule 
--- comparator_24.sv
+++
@@ -1,7 +0,0 @@
-module comparator_24 #(parameter N = 8,M = 23)
-							(input logic [N-1: 0] a,
-							output logic q);
-							
-		assign q = ( a <= M );
-		
-endmodule 
